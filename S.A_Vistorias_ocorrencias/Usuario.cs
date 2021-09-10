a538c2eac6022a4f0b73e0921ff3184e6d94b499
Gabriel Simon <gabrielsimon775@gmail.com>
2021-09-09 21:40:48 -0300

    Implementação das classes Usuario, Vistoriae Ocorrencia e seus atributos

---
+++ S.A_Vistorias_ocorrencias/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace S.A_Vistorias_ocorrencias
+{
+	public class Usuario
+	{
+		public Int32 IdUsuario { get; set; }
+		public string Nome { get; set; }
+		public string login { get; set; }
+		public string senha { get; set; }
+		public string perfil { get; set; }
+	}
+}
---
+++ S.A_Vistorias_ocorrencias/Vistoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace S.A_Vistorias_ocorrencias
+{
+	public class Vistoria
+	{
+		public Int32 IdVistoria { get; set; }
+		public bool Status { get; set; }
+		public DateTime DataAbertura { get; set; }
+		public Int32 IdUsuario { get; set; }
+		public string Imagem { get; set; }
+		public string Descricao { get; set; }
+		public string Endereco { get; set; }
+	}
+}
