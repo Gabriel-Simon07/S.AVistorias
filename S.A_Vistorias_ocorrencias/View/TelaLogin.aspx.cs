a555a4708ed247603abb269265773c85030d584e
Gabriel Simon <gabrielsimon775@gmail.com>
2021-09-17 19:50:04 -0300

    Implementação da tela de Login de usuário

---
+++ S.A_Vistorias_ocorrencias/View/TelaLogin.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace S.A_Vistorias_ocorrencias
+{
+	public partial class TelaLogin : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
