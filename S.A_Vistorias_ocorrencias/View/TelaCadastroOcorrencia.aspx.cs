8e3c233e6a5d492b7b52be2e712fe49774af622b
Gabriel Simon <gabrielsimon775@gmail.com>
2021-09-20 20:30:56 -0300

    Remoção das classes Controller

--- S.A_Vistorias_ocorrencias/View/TelaCadastroOcorrencia.aspx.cs
+++ S.A_Vistorias_ocorrencias/View/TelaCadastroOcorrencia.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace S.A_Vistorias_ocorrencias.View
 {
@@ -12,6 +16,72 @@ namespace S.A_Vistorias_ocorrencias.View
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
+		}
+
+		public static Ocorrencia getOcorrenciaByIdVistoria(Int32 idVistoria)
+		{
+			Ocorrencia ocorrencia = new Ocorrencia();
+
+			MySqlConnection conexao = new MySqlConnection(Functions.ObterConnectionString());
+
+			string query = $"SELECT  * FROM ocorrencia WHERE id_vistoria = '{idVistoria}' LIMIT 1";
+
+			MySqlCommand comando = new MySqlCommand(query, conexao);
+
+			try
+			{
+				conexao.Open();
+
+				MySqlDataReader dadoLido = comando.ExecuteReader();
+
+				while (dadoLido.Read())
+				{
+					ocorrencia = (new Ocorrencia(dadoLido));
+				}
+
+			}
+			catch (SqlException se)
+			{
+				string messageError = se.ToString();
+			}
+			finally
+			{
+				conexao.Close();
+			}
+			return ocorrencia;
+		}
+
+		public static void atualizarOcorrencia(string idOcorrencia, string descricao, Enum tipo)
+		{
+			Ocorrencia ocorrencia = getOcorrenciaByIdVistoria(Int32.Parse(idOcorrencia));
+			ocorrencia.descricao = descricao;
+			ocorrencia.tipo = tipo;
+
+			Functions.AtualizarOcorrencia(ocorrencia);
+		}
+
+		public static void deletarOcorrenciaById(string idOcorrencia)
+		{
+			Ocorrencia ocorrencia = getOcorrenciaByIdVistoria(Int32.Parse(idOcorrencia));
+
+			//Functions.TodasOcorrencias().Remove(ocorrencia);	
+		}
+
+		public static void getListOcorrencias()
+		{
+
+		}
+
+		public static void CadastrarOcorrencia(Int32 idVistoria, string descricao, DateTime data, Enum tipo)
+		{
+			Ocorrencia ocorrencia = new Ocorrencia();
+
+			ocorrencia.idVistoria = idVistoria;
+			ocorrencia.descricao = descricao;
+			ocorrencia.dataOcorrencia = data;
+			ocorrencia.tipo = tipo;
+
+			Functions.SalvarOcorrencia(ocorrencia);
 		}
 	}
 }
--- S.A_Vistorias_ocorrencias/View/TelaLogin.aspx.cs
+++ S.A_Vistorias_ocorrencias/View/TelaLogin.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace S.A_Vistorias_ocorrencias
 {
@@ -67,6 +71,79 @@ namespace S.A_Vistorias_ocorrencias
 
 			if (temError)
 				Response.Write("<script>alert('Erro: " + mensagemErro + " " + usuario + "')<script>");
+		}
+
+		public static Usuario getNameById(string nome)
+		{
+			{
+				Usuario usuario = new Usuario();
+
+				MySqlConnection conexao = new MySqlConnection(Functions.ObterConnectionString());
+
+				string query = $"SELECT usuario.nome FROM usuario WHERE nome = '{nome}' LIMIT 1";
+
+				MySqlCommand comando = new MySqlCommand(query, conexao);
+
+				try
+				{
+					conexao.Open();
+
+					MySqlDataReader dadoLido = comando.ExecuteReader();
+
+					while (dadoLido.Read())
+					{
+						usuario = new Usuario(dadoLido);
+					}
+				}
+				catch (SqlException se)
+				{
+					string messageError = se.ToString();
+				}
+
+				finally
+				{
+					conexao.Close();
+				}
+
+				return usuario;
+			}
+		}
+
+		public static Usuario ObterUsuarioLogin(string login)
+		{
+			Usuario usuario = new Usuario();
+
+			MySqlConnection conexao = new MySqlConnection(Functions.ObterConnectionString());
+
+			string query = $"SELECT * FROM usuario WHERE login = '{login}' LIMIT 1";
+
+			MySqlCommand comando = new MySqlCommand(query, conexao);
+
+			try
+			{
+				conexao.Open();
+
+				MySqlDataReader dadoLido = comando.ExecuteReader();
+				while (dadoLido.Read())
+				{
+					usuario = new Usuario(dadoLido);
+				}
+			}
+			catch (SqlException se)
+			{
+				string messageError = se.ToString();
+			}
+			finally
+			{
+				conexao.Clone();
+			}
+			return usuario;
+		}
+
+		public static bool validaLogin(string login, string senha)
+		{
+			Usuario usuario = ObterUsuarioLogin(login);
+			return usuario.login == login && usuario.senha == senha;
 		}
 	}
 }
